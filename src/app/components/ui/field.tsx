24b0a0d75138228f6a7bcec2258d5f1f01146f42
izileth <kawaklebersc@gmail.com>
2025-10-14 14:19:41 -0300

    Fix Imports

--- src/app/components/ui/field.tsx
+++ src/app/components/ui/field.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "../../lib/utils"
 import { Label } from "./label"
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "./separator"
 
 function FieldSet({ className, ...props }: React.ComponentProps<"fieldset">) {
   return (
